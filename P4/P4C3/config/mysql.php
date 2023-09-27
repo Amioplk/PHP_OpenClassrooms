fecd0d7a62efb95a8ccfea8083c125fc9ac76e9d
Amioplk <amir.worms@dauphine.eu>
2023-09-27 11:06:42 +0200

    First changes to add contibution

--- P4/P4C3/config/mysql.php
+++ P4/P4C3/config/mysql.php
@@ -4,7 +4,7 @@ const MYSQL_HOST = 'localhost';
 const MYSQL_PORT = 3306;
 const MYSQL_NAME = 'we_love_food';
 const MYSQL_USER = 'root';
-const MYSQL_PASSWORD = '';
+const MYSQL_PASSWORD = 'root';
 
 try {
     $mysqlClient = new PDO(
--- P4/P4C3/header.php
+++ P4/P4C3/header.php
@@ -18,6 +18,9 @@
         <li class="nav-item">
           <a class="nav-link" href="contact.php">Contact</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contribute.php">Contribuer</a>
+        </li>
       </ul>
     </div>
   </div>
