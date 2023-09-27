aabe74d897d1fa46e2f15a0c05b0c9d6ec0da322
Amioplk <amir.worms@dauphine.eu>
2023-09-27 11:56:59 +0200

    Clean after submit contribution

--- P4/P4C3/submit_contribution.php
+++ P4/P4C3/submit_contribution.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
 
+include('config/mysql.php');
+
 $postData = $_POST;
 
-print_r($postData);
+// print_r($postData);
 
 // Ecriture de la requête
 $sqlQuery = 'INSERT INTO recipes(title, recipe, author, is_enabled) VALUES (:title, :recipe, :author, :is_enabled)';
 
 // Préparation
-$insertRecipe = $db->prepare($sqlQuery);
+$insertRecipe = $mysqlClient->prepare($sqlQuery);
 
 // Exécution ! La recette est maintenant en base de données
 $insertRecipe->execute([
@@ -21,4 +23,22 @@ $insertRecipe->execute([
 
 ?>
 
-<?php include_once('header.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Recettes - Page d'accueil</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+        <?php include_once('header.php'); ?>
+        <!-- Inclure home.php nous donne un warning "Session strated : ignoring it" Comment faire pour s'en débarasser ? -->
+    </div>
+</body>
+</html>
